dc1ea616be0954b2b3a3eff56265261f91d6dc55
bseony2 <bseony2@gmail.com>
2023-11-17 14:40:25 +0900

    UI 기능 구현 1. 참여자 이름 입력 2. 플레이어 배팅 금액 입력

---
+++ src/main/java/nextstep/blackjack/utils/UserInterface.java
@@ -0,0 +1,24 @@
+package nextstep.blackjack.utils;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static String[] askName() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        return sc.nextLine().split(",");
+    }
+
+    public static int askBatAmt(String name) {
+        int amt = 0;
+        try {
+            System.out.println(name + "의 배팅 금액은?");
+            amt = sc.nextInt();
+        } catch (Error e) {
+            System.out.println(e.getMessage());
+            askBatAmt(name);
+        }
+        return amt;
+    }
+}
