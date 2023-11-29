8461dae334050d63b1570ccb45cba64c2f3666cd
bseony2 <bseony2@gmail.com>
2023-11-29 17:03:52 +0900

    플레이어들의 배팅 금액 확인 기능 추가 플레이어 추가 카드 수령 여부 확인 기능 추가

--- src/main/java/nextstep/blackjack/util/UserInterface.java
+++ src/main/java/nextstep/blackjack/util/UserInterface.java
@@ -1,5 +1,9 @@
 package nextstep.blackjack.util;
 
+import nextstep.blackjack.domain.Player;
+import nextstep.blackjack.enums.UserAnswer;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -10,15 +14,33 @@ public class UserInterface {
         return sc.nextLine().split(",");
     }
 
-    public static int askBatAmt(String name) {
+    public static int askBatAmt(List<Player> players) {
+
+        int totalAmt = 0;
+        for (Player player : players) {
+
+            int batAmt = askBatAmt(player);
+            totalAmt += batAmt;
+        }
+
+        return totalAmt;
+    }
+    private static int askBatAmt(Player player) {
         int amt = 0;
         try {
-            System.out.println(name + "의 배팅 금액은?");
+            System.out.println(player.getName() + "의 배팅 금액은?");
             amt = sc.nextInt();
         } catch (Error e) {
             System.out.println(e.getMessage());
-            askBatAmt(name);
+            askBatAmt(player);
         }
+        player.setBattingMoney(amt);
         return amt;
+    }
+
+    public static UserAnswer askExtrCard(Player player) {
+        System.out.println(player.getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+        String answer = sc.next();
+        return UserAnswer.getAnswer(answer);
     }
 }
