1fdf5c3c912a31f4ad8a5119483c3df5d7cba28d
bseony2 <bseony2@gmail.com>
2023-11-29 17:02:33 +0900

    문양 이름 한글로 수정

--- src/main/java/nextstep/blackjack/enums/Suit.java
+++ src/main/java/nextstep/blackjack/enums/Suit.java
@@ -4,20 +4,26 @@ import java.util.Arrays;
 
 public enum Suit {
 
-    CLUB(0)
-    ,HEART(1)
-    ,DIAMOND(2)
-    ,SPADE(3);
+    CLUB(0, "클로버")
+    ,HEART(1, "하트")
+    ,DIAMOND(2, "다이아몬드")
+    ,SPADE(3, "스페이드");
 
     private final int index;
+    private final String name;
 
-    Suit(int index) {
+    Suit(int index, String name) {
         this.index = index;
+        this.name = name;
     }
 
     public static Suit getSuit(int index) {
         return Arrays.stream(values())
                 .filter(s -> s.index == index)
                 .findAny().orElseThrow(() -> new IllegalArgumentException("문양 인자값을 확인하시기 바랍니다."));
+    }
+
+    public String getName() {
+        return name;
     }
 }
