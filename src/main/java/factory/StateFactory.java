c0d087c7f54ac38249fed7bd26b52d21ee81ca82
bseony2 <bseony2@gmail.com>
2023-12-20 14:28:03 +0900

    초기 2장의 카드를 받아 상태를 생성하는 팩토리 추가

---
+++ src/main/java/factory/StateFactory.java
@@ -0,0 +1,23 @@
+package factory;
+
+import domain.Cards;
+import state.Blackjack;
+import state.Bust;
+import state.Hit;
+import state.State;
+
+public class StateFactory {
+
+    private static final int BLACKJACK_SCORE = 21;
+
+    public static State stateGenerate(Cards cards) {
+        int score = cards.getScore();
+
+        if (score < BLACKJACK_SCORE) {
+            return new Hit(cards);
+        }
+
+        return new Blackjack(cards);
+
+    }
+}
